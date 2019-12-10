fd81c2346cf65ad7485dfd7af9561ebe04621bd1
David Wilkie <dwilkie@gmail.com>
2019-12-10 20:03:55 +0700

    WIP

--- .github/actions/update_pumi/entrypoint.sh
+++ .github/actions/update_pumi/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh -l
 
+set -e
+set -o pipefail
+
+touch "csv_data/provinces.csv"
 for province_code in `seq 1 2`
 do
-   curl -s "http://db.ncdd.gov.kh/gazetteer/province/downloadprovince.castle?pv=$province_code" > "p$province_code.xls"
-   ssconvert "p$province_code.xls" "csv_data/p$province_code.csv"
+  curl -s "http://db.ncdd.gov.kh/gazetteer/province/downloadprovince.castle?pv=$province_code" > "p$province_code.xls"
+  ssconvert "p$province_code.xls" "csv_data/p$province_code.csv"
+  cat "csv_data/provinces.csv" "csv_data/p$province_code.csv" >> "csv_data/provinces.csv"
 done
+
+cat "csv_data/provinces.csv"
