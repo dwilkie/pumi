9e15b7619907bb301e1f41410bb66a55e3659a91
David Wilkie <dwilkie@gmail.com>
2019-12-10 19:19:56 +0700

    WIP

--- .github/actions/update_pumi/entrypoint.sh
+++ .github/actions/update_pumi/entrypoint.sh
@@ -2,6 +2,7 @@
 
 for i in {1..5}
 do
+   echo "http://db.ncdd.gov.kh/gazetteer/province/downloadprovince.castle?pv=$i"
    curl -s "http://db.ncdd.gov.kh/gazetteer/province/downloadprovince.castle?pv=$i" > "pv$i.xls"
    ssconvert "pv$i.xls" "csv/pv$i.csv"
    cat "csv/pv$i.csv"
