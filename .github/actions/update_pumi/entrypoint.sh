4a809c4531020ec0e458b8a00c3066c1284195f3
David Wilkie <dwilkie@gmail.com>
2019-12-10 18:50:10 +0700

    WIP

--- .github/actions/update_pumi/entrypoint.sh
+++ .github/actions/update_pumi/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+mkdir -p csv
+curl -s "http://db.ncdd.gov.kh/gazetteer/province/downloadprovince.castle?pv=1" > p1.xls
+ssconvert p1.xls csv/p1.csv
+cat csv/p1.csv
