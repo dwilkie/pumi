0ff3947ed6c239a573ca51e6eee9f020bf8d433c
David Wilkie <dwilkie@gmail.com>
2019-12-11 09:33:31 +0700

    WIP

---
+++ .github/actions/update_data/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+set -o pipefail
+
+for province_index in `seq 1 25`
+do
+  curl -s "http://db.ncdd.gov.kh/gazetteer/province/downloadprovince.castle?pv=$province_index" > "tmp/p$province_index.xls"
+  ssconvert "tmp/p$province_index.xls" "tmp/p$province_index.csv"
+done
