ab06018c19f1546c8d1c2c53b154623037301563
David Wilkie <dwilkie@gmail.com>
2023-02-06 10:13:22 +0700

    feat: Update data (#33)

--- spec/pumi/district_spec.rb
+++ spec/pumi/district_spec.rb
@@ -6,7 +6,7 @@ module Pumi
       it "returns all districts" do
         results = District.all
 
-        expect(results.size).to eq(204)
+        expect(results.size).to eq(208)
         expect(results.first).to be_a(District)
       end
     end
