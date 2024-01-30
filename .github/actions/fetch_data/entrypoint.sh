531951435e48039ef8179cb19ffbf053c6ec4472
David Wilkie <dwilkie@gmail.com>
2024-01-30 19:43:48 +0700

    WIP

--- .github/actions/fetch_data/entrypoint.sh
+++ .github/actions/fetch_data/entrypoint.sh
@@ -3,13 +3,17 @@
 set -e
 set -o pipefail
 
-URL="https://db.ncdd.gov.kh/gazetteer/province/downloadprovince.castle?pv=PROVINCE_CODE"
-PLACEHOLDER="PROVINCE_CODE"
-NUM_PROVINCES="25"
+if [ "$1" = 'fetch-data' ]; then
+  URL="https://db.ncdd.gov.kh/gazetteer/province/downloadprovince.castle?pv=PROVINCE_CODE"
+  PLACEHOLDER="PROVINCE_CODE"
+  NUM_PROVINCES="25"
 
-for province_code in `seq 1 $NUM_PROVINCES`
-do
-  province_url=$(echo $URL | sed -e s/$PLACEHOLDER/$province_code/g)
-  curl -s $province_url > "tmp/p$province_code.xls"
-  ssconvert "tmp/p$province_code.xls" "tmp/p$province_code.csv"
-done
+  for province_code in `seq 1 $NUM_PROVINCES`
+  do
+    province_url=$(echo $URL | sed -e s/$PLACEHOLDER/$province_code/g)
+    curl -k -s $province_url > "tmp/p$province_code.xls"
+    ssconvert "tmp/p$province_code.xls" "tmp/p$province_code.csv"
+  done
+else
+  exec "$@"
+fi
